a41190440d7eba7e42dee2b6a537125c1ff5343e
LBEBEN <lukas.beben@gmail.com>
2022-02-13 19:38:45 +0100

    Poprawki z 13.02.2022

--- JAVA/src/pl/coderslab/TaskManager.java
+++ JAVA/src/pl/coderslab/TaskManager.java
@@ -11,51 +11,16 @@ import java.util.Scanner;
 public class TaskManager {
 
     public static void main(String[] args) {
-        // Zapisanie danych z pliku do tablicy dwuwymiarowej
-        tasks();
+        // wyświtlenie opcji do wyboru
+        desription();
 
-        // wyświtlanie opcji do wyboru na konsoli
-        System.out.println(ConsoleColors.BLUE + "Wybierz co chesz zrobić:");
-        String[] option = new String[]{"Add - dodawanie nowego zadania", "Remove - usuwanie zadania", "List - wypisanie wszystkich zadań", "Exit - wyjście z programu"};
-        for (String s : option) {
-            System.out.println(ConsoleColors.RESET + s + "\n");
-        }
-
-        System.out.println("Wybierz co chcesz wykonać");
-
-        Scanner wybor = new Scanner(System.in);
-        String choice = wybor.next();
-        while (!choice.equals("Exit")) {
-
-            switch (choice) {
-                //case "Add": Add();
-                //case "Remove": Remove();
-                case "List":
-                    wyświetl(tasks());
-                    break;
-                default:
-                    System.out.println("Wybierz polecenie z listy");
-            }
-            choice = wybor.next();
-
-        }
-    }
-
-    //Metoda służąca wczytywaniu danych z pliku do dwuwymiarowej tablicy
-    public static String[][] tasks() {
+        // tworzenie tablicy dwuwymiarowej
         Path sciezka = Paths.get("tasks.csv");
         Scanner scan = null;
         try {
             scan = new Scanner(sciezka);
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //sprawdzam czy plik istnieje
-        if (Files.exists(sciezka)) {
-            System.out.println("");
-        } else {
-            System.out.println("coś poszło nie tak");
+            System.out.println("Plik nie istnieje");;
         }
 
         int line = 0; // ilość lini w pliku tasks;
@@ -64,18 +29,56 @@ public class TaskManager {
             scan.nextLine();
             line++;
         }
-
         String[][] tab = new String[line][3];
 
-        Scanner scan2 = null;
+        // wpisywanie danych z pliku csv do tablicy dwuwymiarowej
         try {
-            scan2 = new Scanner(sciezka);
-        } catch (IOException e) {
-            e.printStackTrace();
+            tabZpliku(tab);
         }
+        catch (IOException e){
+            System.out.println("Plik nie istnieje!");
+        }
+        // użytkownik wybiera jaką operację chce wykonać
+        System.out.println(ConsoleColors.BLUE + "Wybierz jedną z dostępnych opcji:" + ConsoleColors.RESET);
+        Scanner skanujOpcje = new Scanner(System.in);
+        String wybranaOpcja = skanujOpcje.next();
+        while (!wybranaOpcja.equals("exit")){
+        switch (wybranaOpcja){
+            case "add" : add(tab); break;
+            //case remove: ;
+            case "list" : list(tab); break;
+        }
+            System.out.println(ConsoleColors.BLUE + "Wybierz kolejną czynność:" + ConsoleColors.RESET);
+        wybranaOpcja = skanujOpcje.next();
+        }
+
+
+
+
+    }
+
+// metoda służąca wyświetlainu opcji do wyboru
+    public static void desription(){
+        System.out.println(ConsoleColors.BLUE +"Program posiada poniższe funkcjonalności:");
+        String[] opcje = new String[] {
+                "add - dodaj zadania do realizacji",
+                "remove - usuń zdanie",
+                "list - wyświetl zadania do realizacji",
+                "exit - zapisz i zamknij program"
+        };
+        for (String s : opcje) {
+            System.out.println(ConsoleColors.RESET + s);
+        }
+        System.out.println("");
+    }
+    // metoda służąca wpisywaniu danych z pliku do tablicy dwuwymiarowej
+    public static String [][] tabZpliku (String[][] tab) throws IOException {
+        Path sciezka = Paths.get("tasks.csv");
+        Scanner scan = new Scanner(sciezka);
         int wiersz = 0;
-        while (scan2.hasNextLine()) {
-            data = scan2.nextLine();
+        String data = "";
+        while (scan.hasNextLine()) {
+            data = scan.nextLine();
             String[] data1 = data.split(", ");
             for (int i = 0; i < data1.length; i++) {
                 tab[wiersz][i] = data1[i];
@@ -84,29 +87,60 @@ public class TaskManager {
         }
         return tab;
     }
-
-    // Metoda wyświetająca zawartość pliku na konsoli
-    public static void wyświetl(String[][] tab) {
+// metoda służąca wyświetleniu tego co jest w tabeli dwuwymiarowej
+    public static void list(String[][] tab) {
         for (int i = 0; i < tab.length; i++) {
+            System.out.print (i + ":");
             for (int j = 0; j < tab[i].length; j++) {
                 System.out.print(tab[i][j] + " ");
             }
             System.out.println("");
+        }}
+    public static String[][] add(String[][] tab){
+        tab = Arrays.copyOf(tab, tab.length+1);
+        tab[tab.length-1] = new String[3];
+        for(int i=0; i< tab.length;i++){
+            for(int j=0; j<tab[i].length; j++){
+                System.out.print(tab[i][j] + " ");
+            }
+            System.out.println("");
         }
+        System.out.println("Podaj opis zadania:");
+        Scanner zadanie = new Scanner(System.in);
+        String zadanie1= zadanie.nextLine();
+        tab[tab.length-1][0] = zadanie1;
+        System.out.println("Podaj date wykonania zadania:");
+        Scanner data = new Scanner(System.in);
+        tab[tab.length-1][1] = data.nextLine();
+        System.out.println("Czy to zadanie jest ważne [podaj true/false]:");
+        Scanner important = new Scanner(System.in);
+        tab[tab.length-1][2] = important.nextLine();
+        tab[tab.length-1] = new String[3];
+        for(int i=0; i< tab.length;i++){
+            for(int j=0; j<tab[i].length; j++){
+                System.out.print(tab[i][j] + " ");
+            }
+            System.out.println("");
+        }
+        return tab;
     }
+
+
+}
+   /* //Metoda służąca wczytywaniu danych z pliku do dwuwymiarowej tablicy
+    public static String[][] tasks() {
+
 
     public static void EXIT(String[][] tab) {
         Path sciezka = Paths.get("tasks.csv");
+String [] jedenWymiar = new String[tab.length];
 
+*//*
 
-        try {
-            Files.write(sciezka, tab);
-        } catch (IOException e) {
-            System.out.println("Plik nie istnieje");
-        }
+*//*
 
     }
-}
+}*/
 
 
 
